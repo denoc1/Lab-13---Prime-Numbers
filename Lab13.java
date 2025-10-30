d7e92e920669c3002ef36d77928c2ac7ce961aed
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-10-30 11:59:01 +0000

    Template

---
+++ Lab13.java
@@ -0,0 +1,10 @@
+public class Lab13{
+//put your three static methods here - don't forget documentation
+
+
+//main method here should call the methods
+    public static void main(String[] args){
+
+
+    }//end main
+}//end class
